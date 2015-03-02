6f78abd78c8b16c9b9a79f27b0c09a1dddac8183
Kelsey <kma3a@virginia.edu>
2015-03-01 20:48:17 -0500

    added so that the user will redirect to correct location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(linkedin)
+    if current_user.sign_in_count == 1
+      new_user_path(current_user)
+    else
+      user_path(current_user)
+    end
+  end
 end
