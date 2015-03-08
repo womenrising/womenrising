0b7c2d8ebb17e1e491f01963e6676bae85d76865
Kelsey <kma3a@virginia.edu>
2015-03-08 19:46:51 -0400

    adding fix to the path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update(first_name:params[:user][:first_name],last_name:params[:user][:last_name],mentor:params[:user][:mentor],primary_industry:params[:user][:primary_industry],stage_of_career:params[:user][:stage_of_career],mentor_industry:params[:user][:mentor_industry],current_goal:params[:user][:current_goal],top_3_interests:params[:user][:top_3_interests],live_in_detroit:params[:user][:live_in_detroit])
-      redirect_to 'show'
+      redirect_to user_path(current_user)
     else
       render 'edit'
     end
