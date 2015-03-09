e2eaa4a23669971ef37490368894a6aaccffb722
Kelsey <kma3a@virginia.edu>
2015-03-08 22:32:40 -0400

    adding changes to edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @industries = ["Business", "Technology", "Startup","Other"]
     @user = current_user
   end
 
   def update
     @user = current_user
-    if @user.update(first_name:params[:user][:first_name],last_name:params[:user][:last_name],mentor:params[:user][:mentor],primary_industry:params[:user][:primary_industry],stage_of_career:params[:user][:stage_of_career],mentor_industry:params[:user][:mentor_industry],current_goal:params[:user][:current_goal],top_3_interests:params[:user][:top_3_interests],live_in_detroit:params[:user][:live_in_detroit])
+    if @user.update(first_name:params[:user][:first_name],last_name:params[:user][:last_name],mentor:params[:user][:mentor],primary_industry:params[:user][:primary_industry],stage_of_career:params[:user][:stage_of_career],mentor_industry:params[:user][:mentor_industry], peer_industry:params[:user][:peer_industry],current_goal:params[:user][:current_goal],top_3_interests:params[:user][:top_3_interests],live_in_detroit:params[:user][:live_in_detroit])
       redirect_to user_path(current_user)
     else
       render 'edit'
