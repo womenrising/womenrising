5d32cad6149fe3f5f6f4a5e3eac2c05b5e99b41e
Kelsey <kma3a@virginia.edu>
2015-03-22 17:05:59 -0400

    adding gems to help create seed file and adding another part to the database to check if the person has been assigned to a peer groups

--- db/migrate/20150302002618_add_mentor_industry_stage_of_career_waitlist_questions_to_users.rb
+++ db/migrate/20150302002618_add_mentor_industry_stage_of_career_waitlist_questions_to_users.rb
@@ -8,7 +8,8 @@ class AddMentorIndustryStageOfCareerWaitlistQuestionsToUsers < ActiveRecord::Mig
     add_column :users, :current_goal, :string
     add_column :users, :top_3_interests, :text, array:true, default: []
     add_column :users, :live_in_detroit, :boolean, default:true
-    add_column :users, :waitlist, :boolean, default:false
+    add_column :users, :waitlist, :boolean, default:true
     add_column :users, :is_participating_next_month, :boolean, default:false
+    add_column :users, :is_asssined_peer, :boolean, default:false
   end
 end
