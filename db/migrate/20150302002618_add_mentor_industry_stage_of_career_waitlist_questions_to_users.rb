837818bb000b9b3ed11f8122ac433afecab91c62
Kelsey <kma3a@virginia.edu>
2015-03-22 15:54:47 -0400

    adding the storage in the database to store if they will be participating the next month

--- db/migrate/20150302002618_add_mentor_industry_stage_of_career_waitlist_questions_to_users.rb
+++ db/migrate/20150302002618_add_mentor_industry_stage_of_career_waitlist_questions_to_users.rb
@@ -9,5 +9,6 @@ class AddMentorIndustryStageOfCareerWaitlistQuestionsToUsers < ActiveRecord::Mig
     add_column :users, :top_3_interests, :text, array:true, default: []
     add_column :users, :live_in_detroit, :boolean, default:true
     add_column :users, :waitlist, :boolean, default:false
+    add_column :users, :is_participating_next_month, :boolean, default:false
   end
 end
