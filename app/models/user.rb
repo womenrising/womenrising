f787abb4ad77fc77393deef19953e76d744c9f18
Kelsey <kma3a@virginia.edu>
2015-03-28 18:53:57 -0400

    adjusting the spelling of many because I spelled it wrong

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :mentors, class_name: "mentor", foreign_key: 'mentor_id'
-  has_may :mentees, class_name: "mentor", foreign_key: 'mentee_id'
+  has_many :mentees, class_name: "mentor", foreign_key: 'mentee_id'
  validates :top_3_interests, length: {maximum: 3, too_long: " is limited to %{count} interests"}
  validates_presence_of :first_name, :last_name
  validates_presence_of :mentor_industry, if: :mentor
