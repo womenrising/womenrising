4ea70cd47cd1e30efb38b9580e0aca30041dbb48
Kelsey <kma3a@virginia.edu>
2015-03-28 19:57:55 -0400

    adding the changes to the models so that it works

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,5 +1,5 @@
 class Mentor < ActiveRecord::Base
 
   belongs_to :mentee, class_name: "User", foreign_key: 'mentee_id'
-  belongs_to :mentor, class_name: "User", foreign_key: 'mentor_id'
+  belongs_to :mentoring, class_name: "User", foreign_key: 'mentor_id'
 end
