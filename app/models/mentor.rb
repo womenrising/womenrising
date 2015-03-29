542208949bf527d30f6d455bb4f4c32d48d67544
Kelsey <kma3a@virginia.edu>
2015-03-28 23:59:37 -0400

    adding the filter to choose the mentor

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -9,12 +9,12 @@ class Mentor < ActiveRecord::Base
   end
 
   def get_possible_mentors
-    User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", current_user.stage_of_career, 0).where(mentor_industry: current_user.primary_industry)
+    User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", mentee.stage_of_career, 0).where(mentor_industry: mentee.primary_industry)
   end
 
   def previous_mentors
     all_mentors = []
-    current_user.mentees.each do |mentor|
+    mentee.mentees.each do |mentor|
       all_mentors << mentor
     end
     all_mentors
