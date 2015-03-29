d46e06f5a5a6ddcd8a59bf68e6664de28c4f2d39
Kelsey <kma3a@virginia.edu>
2015-03-28 22:36:22 -0400

    adding routes for mentoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}
   resources :users
+  resources :mentors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
