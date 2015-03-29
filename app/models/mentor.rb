4723468eec1a46995d6960acb2adb2ca48a0326f
Kelsey <kma3a@virginia.edu>
2015-03-28 22:25:44 -0400

    adding the previous mentors to get the previous mentors

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -6,4 +6,12 @@ class Mentor < ActiveRecord::Base
   def get_possible_mentors(industry)
     User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", current_user.stage_of_career, 0).where(mentor_industry: industry)
   end
+
+  def previous_mentors
+    all_mentors = []
+    current_user.mentees.each do |mentor|
+      all_mentors << mentor
+    end
+    all_mentors
+  end
 end
