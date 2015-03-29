32f91e10f5c46f4275446073fdc2c998dd6b741e
Kelsey <kma3a@virginia.edu>
2015-03-28 23:53:15 -0400

    adding validation to the model for mentoring

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -4,8 +4,12 @@ class Mentor < ActiveRecord::Base
   belongs_to :mentee, class_name: "User", foreign_key: 'mentee_id'
   belongs_to :mentoring, class_name: "User", foreign_key: 'mentor_id'
 
-  def get_possible_mentors(industry)
-    User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", current_user.stage_of_career, 0).where(mentor_industry: industry)
+  before_validation(on: :create) do
+    self.mentor_id = choose_mentor.id
+  end
+
+  def get_possible_mentors
+    User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", current_user.stage_of_career, 0).where(mentor_industry: current_user.primary_industry)
   end
 
   def previous_mentors
