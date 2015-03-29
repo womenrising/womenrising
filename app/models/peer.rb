d1fb84f3350780063f8bb0d65981aff9e5dca125
Kelsey <kma3a@virginia.edu>
2015-03-29 19:06:51 -0400

    adding associations to the peer model

--- app/models/peer.rb
+++ app/models/peer.rb
@@ -1,2 +1,6 @@
 class Peer < ActiveRecord::Base
+  belongs_to :peer1, class_name: "User", foreign_key: 'peer1_id'
+  belongs_to :peer2, class_name: "User", foreign_key: 'peer2_id'
+  belongs_to :peer3, class_name: "User", foreign_key: 'peer3_id'
+
 end
