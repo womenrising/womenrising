84b04d6fc86ff7087af0ba328b6653315dcb211a
Kelsey <kma3a@virginia.edu>
2015-03-28 22:15:29 -0400

    adding the method to get all the possible mentors and adjusted the seed for the user

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,4 +2,8 @@ class Mentor < ActiveRecord::Base
 
   belongs_to :mentee, class_name: "User", foreign_key: 'mentee_id'
   belongs_to :mentoring, class_name: "User", foreign_key: 'mentor_id'
+
+  def get_possible_mentors(industry)
+    User.where(is_participating_next_month: true, mentor: true, waitlist: false).where("stage_of_career > ? AND mentor_times > ?", current_user.stage_of_career, 0).where(mentor_industry: industry)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Fabricator(:user) do
   email { Faker::Internet.email}
   password {"testtingstuff"}
@@ -15,12 +14,14 @@ Fabricator(:user) do
   mentor {%w(true false).sample}
   primary_industry {["Business", "Technology", "Startup","Other"].sample}
   stage_of_career {%w(1 2 3 4 5).sample}
-  mentor_industry {["Business", "Technology", "Startup","Other"].sample}
-  peer_industry {["Business", "Technology", "Startup","Other"].sample}
+  mentor_industry {["Business", "Technology", "Startup"].sample}
+  peer_industry {["Business", "Technology", "Startup"].sample}
   current_goal {["Rising the ranks / breaking the glass ceiling","Switching industries","Finding work/life balance"].sample}
   top_3_interests {["Arts", "Music", "Crafting", "Home improvement / Decorating", "Being a mom", "Dogs", "Cats", "Watch    ing Sports", "Outdoors / Hiking", "Exercise", "Biking", "Yoga", "Running", "Beer","Wine","Traveling"," Local events",    "Reading", "Photography", "Movies","Cooking / Eating / Being a foodie" ,"Social issues / volunteering","Video Games"].sample(3)}
   live_in_detroit {%w(true false).sample}
   is_participating_next_month {%w(true false).sample}
 end
 
-50.times {Fabricate(:user)}
+150.times { Fabricate(:user)}
+
+
