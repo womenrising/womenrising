cb3d3b58132d65a1513500dfd3bab38a4d4f2d1c
Kelsey <kma3a@virginia.edu>
2015-03-29 15:42:08 -0400

    adding method in mailers so the mentee will get notified about who the mentor is

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,12 @@ class UserMailer < ActionMailer::Base
     @mentee = mentor_match.mentee
     mail(to: @mentor.email, subject: "#{mentee.first_name} has requested to learn with you")
   end
+
+  def mentee_mail(mentor_match)
+    @mentor_match = mentor_match
+    @mentor = mentor_match.mentoring
+    @mentee = mentor_match.mentee
+    mail(to: @mentee.email, subject: "You have requested to be mentored by #{@mentor.first_name}")
+  end
+
 end
