991508dc1f0163f158f4b7560a5b3cac6c2677ee
Kelsey <kma3a@virginia.edu>
2015-03-29 14:35:41 -0400

    adding so we can get the information about the mentoring to the mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "info@womenrising.co"
 
-  def mentoring(mentor, mentee)
-    @mentor = mentor
-    @mentee = mentee
+  def mentoring(mentor_match)
+    @mentor_match = mentor_match
+    @mentor = mentor_match.mentoring
+    @mentee = mentor_match.mentee
     mail(to: @mentor.email, subject: "#{mentee.first_name} has requested to learn with you")
   end
 end
