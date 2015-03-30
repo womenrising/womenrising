aa7b284d389a52658302b7ac82447f5f710582e2
Kelsey <kma3a@virginia.edu>
2015-03-29 21:03:56 -0400

    starting to write out the algorithem to get the peers

--- app/models/peer.rb
+++ app/models/peer.rb
@@ -3,4 +3,19 @@ class Peer < ActiveRecord::Base
   belongs_to :peer2, class_name: "User", foreign_key: 'peer2_id'
   belongs_to :peer3, class_name: "User", foreign_key: 'peer3_id'
 
+
+  def get_all_peers
+    @all_avalible = User.where(live_in_detroit:true, is_participating_next_month: true, waitlist: false, is_assigned_peer_group: false)
+  end
+
+  def choose_first_peer
+    @first_peer = get_all_peers.sample
+  end
+
+  def get_ther_peers
+    min = @first_peer.stage_of_career > 1 ? (@first_peer.stage_of_career - 1) : 1
+    max = @first_peer.stage_of_career < 5 ? (@first_peer.stage_of_career + 1) : 5
+
+    @peers = User.where(live_in_detroit:true, is_participating_next_month: true, waitlist: false, is_assigned_peer_group: false, peer_industry: @first_peer.peer_industry).where('stage_of_career >= ? AND stage_of_career <= ?', min, max)
+    end
 end
