9dd1446a8f19db526480f2a6e251179ae0b7e32d
Kelsey <kma3a@virginia.edu>
2015-04-05 14:38:43 -0400

    adding test to make sure that update_month works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe User, :type => :model do
   it{should have_many(:peer2).class_name('Peer').with_foreign_key('peer2_id')}
   it{should have_many(:peer3).class_name('Peer').with_foreign_key('peer3_id')}
   it{should have_many(:peer4).class_name('Peer').with_foreign_key('peer4_id')}
+
+  context "#update_month" do
+  	it "should update the users is_participating_this_month, is_participating_next_month, mentor_times, is_assigned_peer_group " do
+  		current_user = User.create!(email: "hellowerd2@gmail.com", password: "Somethingwierd12",password_confirmation: "Somethingwierd12", first_name: "Hello",last_name: "world", mentor: true, primary_industry: "Technology", stage_of_career: 5, mentor_industry: "Technology", peer_industry: ["Business", "Technology", "Startup"].sample, current_goal: ["Rising the ranks / breaking the glass ceiling","Switching industries","Finding work/life balance"].sample,top_3_interests: ["Arts", "Music", "Crafting", "Home improvement / Decorating", "Being a mom", "Dogs", "Cats", "Watching Sports", "Outdoors / Hiking", "Exercise", "Biking", "Yoga", "Running", "Beer","Wine","Traveling"," Local events",    "Reading", "Photography", "Movies","Cooking / Eating / Being a foodie" ,"Social issues / volunteering","Video Games"].sample(3), live_in_detroit: %w(true false).sample, is_participating_next_month: true, is_participating_this_month: false,
+ 				mentor_times: 0, mentor_limit: 1, is_assigned_peer_group: true)
+  		User.update_month
+  		current_user = User.find(current_user.id)
+  		expect(current_user.is_participating_next_month).to eq(false)
+  		expect(current_user.mentor_times).to eq(1)
+  		expect(current_user.is_participating_this_month).to eq(true)
+  		expect(current_user.is_participating_next_month).to eq(false)
+  		expect(current_user.is_assigned_peer_group).to eq(true)
+  	end
+  end
 end
