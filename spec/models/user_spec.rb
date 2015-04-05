601a2c7f99a5a2b5bd389ccad21cfdd7a009b2d2
Kelsey <kma3a@virginia.edu>
2015-04-05 15:00:00 -0400

    adding test for if the primary industry is nil it will be on the waitlist

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, :type => :model do
   it{should have_many(:peer3).class_name('Peer').with_foreign_key('peer3_id')}
   it{should have_many(:peer4).class_name('Peer').with_foreign_key('peer4_id')}
 
+  before{10.times{FactoryGirl.create(:user)}}
   context "#update_month" do
   	it "should update the users is_participating_this_month, is_participating_next_month, mentor_times, is_assigned_peer_group " do
   		current_user = User.create!(email: "hellowerd2@gmail.com", password: "Somethingwierd12",password_confirmation: "Somethingwierd12", first_name: "Hello",last_name: "world", mentor: true, primary_industry: "Technology", stage_of_career: 5, mentor_industry: "Technology", peer_industry: ["Business", "Technology", "Startup"].sample, current_goal: ["Rising the ranks / breaking the glass ceiling","Switching industries","Finding work/life balance"].sample,top_3_interests: ["Arts", "Music", "Crafting", "Home improvement / Decorating", "Being a mom", "Dogs", "Cats", "Watching Sports", "Outdoors / Hiking", "Exercise", "Biking", "Yoga", "Running", "Beer","Wine","Traveling"," Local events",    "Reading", "Photography", "Movies","Cooking / Eating / Being a foodie" ,"Social issues / volunteering","Video Games"].sample(3), live_in_detroit: %w(true false).sample, is_participating_next_month: true, is_participating_this_month: false,
@@ -18,7 +19,6 @@ RSpec.describe User, :type => :model do
   		expect(current_user.mentor_times).to eq(1)
   		expect(current_user.is_participating_this_month).to eq(true)
   		expect(current_user.is_participating_next_month).to eq(false)
-  		expect(current_user.is_assigned_peer_group).to eq(true)
   	end
   end
 
@@ -33,6 +33,12 @@ RSpec.describe User, :type => :model do
   		user = User.create!(email: "hellowerd2@gmail.com", password: "Somethingwierd12",password_confirmation: "Somethingwierd12", first_name: "Hello",last_name: "world", mentor: true, primary_industry: "Technology", stage_of_career: 5, mentor_industry: "Technology", peer_industry: ["Business", "Technology", "Startup"].sample, current_goal: ["Rising the ranks / breaking the glass ceiling","Switching industries","Finding work/life balance"].sample,top_3_interests: ["Arts", "Music", "Crafting", "Home improvement / Decorating", "Being a mom", "Dogs", "Cats", "Watching Sports", "Outdoors / Hiking", "Exercise", "Biking", "Yoga", "Running", "Beer","Wine","Traveling"," Local events",    "Reading", "Photography", "Movies","Cooking / Eating / Being a foodie" ,"Social issues / volunteering","Video Games"].sample(3), live_in_detroit: %w(true false).sample, is_participating_next_month: true, is_participating_this_month: false,
  				mentor_times: 0, mentor_limit: 1, is_assigned_peer_group: true)
   		expect(user.waitlist).to eq(false)
+  	end
+
+  	it "should be true if everything primary industry is nil" do
+  		user = User.where(waitlist: false).sample
+  		user.update(primary_industry:nil)
+  		expect(user.waitlist).to eq(true)
   	end
   end
 end
