48e698e7476afae44017597afa1d142ea6d83491
Kelsey <kma3a@virginia.edu>
2015-04-05 12:43:22 -0400

    adding tests for the mentor belings to

--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Mentor, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 	it{should belong_to(:mentee).class_name('User').with_foreign_key('mentee_id')}
+ 	it{should belong_to(:mentoring).class_name('User').with_foreign_key('mentor_id')}
 end
