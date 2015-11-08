4078cec5fd4c75e2fcd5da5f6a2b3c1ec50fa4ec
Kelsey <kma3a@virginia.edu>
2015-11-07 23:38:43 -0500

    adding the code that will change the mentor times for each person if they want to add or remove mentor slots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    if @user.update(first_name:params[:user][:first_name],last_name:params[:user][:last_name],mentor:params[:user][:mentor],mentor_limit:params[:user][:mentor_limit],primary_industry:params[:user][:primary_industry],stage_of_career:params[:user][:stage_of_career],mentor_industry:params[:user][:mentor_industry], peer_industry:params[:user][:peer_industry],current_goal:params[:user][:current_goal],top_3_interests:params[:user][:top_3_interests] || [],live_in_detroit:params[:user][:live_in_detroit])
+    if @user.update(first_name:params[:user][:first_name],last_name:params[:user][:last_name],mentor:params[:user][:mentor],mentor_limit:params[:user][:mentor_limit], mentor_times: @user.mentor_times_change(params[:user][:mentor_limit]), primary_industry:params[:user][:primary_industry],stage_of_career:params[:user][:stage_of_career],mentor_industry:params[:user][:mentor_industry], peer_industry:params[:user][:peer_industry],current_goal:params[:user][:current_goal],top_3_interests:params[:user][:top_3_interests] || [],live_in_detroit:params[:user][:live_in_detroit])
       redirect_to user_path(current_user)
     else
       @industries = ["Business", "Technology", "Startup"]
