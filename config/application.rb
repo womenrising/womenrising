ed97d7ccbba659e558957c6b0d88ba913709d32f
Weston Platter <westonplatter@gmail.com>
2016-07-02 15:21:33 -0600

    rails configs. require rails specific parts (ie, skip test unit)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# leave out TestUnite because we're using RSpec
 
 Bundler.require(*Rails.groups)
 
@@ -10,6 +18,12 @@ module Womenrising
     # auto load the lib folder
     config.autoload_paths << Rails.root.join('lib')
 
+    config.generators do |g|
+      g.test_framework :rspec
+      g.assets = false
+      g.helper = false
+      g.view_specs = false
+    end
   end
 end
 
