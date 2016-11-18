3708671d15874efa84e9735a95e1679dff4fb4d4
Annie Lydens <aelydens@gmail.com>
2016-11-18 07:44:48 -0700

    Remove unused but confusing is_participating_next_month

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20161122011004) do
 
   # These are extensions that must be enabled in order to support this database
