0a178512049ac952c575b5a555871d81052089a1
caward12 <ward.colleen.a@gmail.com>
2017-07-16 14:28:05 -0600

    add endpoint for users show
    add serializer for user
    passing test

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      namespace :users do
-        get '/:id', to: 'users#show'
-      end
+      resources :users, only: [:show]
     end
   end
 end
--- spec/requests/api/v1/users/user_show_request_spec.rb
+++ spec/requests/api/v1/users/user_show_request_spec.rb
@@ -9,9 +9,9 @@ describe 'users returns info for single user' do
     expect(response).to be_success
 
     user = JSON.parse(response.body)
-
-    expect(user.first_name).to eq(users.first.first_name)
-    expect(user.last_name).to eq(users.first.last_name)
-    expect(user.is_participating_this_month).to eq(users.first.is_participating_this_month)
+    
+    expect(user["first_name"]).to eq(users.first.first_name)
+    expect(user["last_name"]).to eq(users.first.last_name)
+    expect(user["is_participating_this_month"]).to eq(users.first.is_participating_this_month)
   end
 end
