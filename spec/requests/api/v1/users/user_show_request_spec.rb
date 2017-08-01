aecccd25711da9385430d75a29dff439a0f4c077
caward12 <ward.colleen.a@gmail.com>
2017-07-31 19:19:38 -0600

    update test and user serializer
    add city method on user to return locaiton.city

--- spec/requests/api/v1/users/user_show_request_spec.rb
+++ spec/requests/api/v1/users/user_show_request_spec.rb
@@ -1,18 +1,33 @@
 require 'rails_helper'
 
 describe 'users returns info for single user' do
-  it 'returns user first name, last name, is participating and peers for given user id' do
-    users = create_list(:user, 3)
+  let(:location) { create :location }
+  let!(:user) { create :user, :groupable, location: location }
+  let!(:user_in_current_group) { create :user, :groupable, location: location }
+  let!(:user_in_other_group) { create :user, :groupable }
 
-    get "/api/v1/users/#{users.first.id}"
+  before do
+    login_as(user, scope: :user)
+    PeerGroup.generate_groups
+  end
+
+  it 'returns user first name, last name, city, and peers for given user id' do
+    get "/api/v1/users/#{user.id}"
 
     expect(response).to be_success
 
-    user = JSON.parse(response.body)
-
-    expect(user["first_name"]).to eq(users.first.first_name)
-    expect(user["last_name"]).to eq(users.first.last_name)
-    expect(user["is_participating_this_month"]).to eq(users.first.is_participating_this_month)
-    expect(user["peers"]).to eq(users.first.peers)
+    parsed_user = JSON.parse(response.body)
+binding.pry
+    expect(parsed_user).to be_a(Hash)
+    expect(parsed_user["first_name"]).to eq(user.first_name)
+    expect(parsed_user["last_name"]).to eq(user.last_name)
+    expect(parsed_user["city"]).to eq(user.city)
+    expect(parsed_user["peers"]).to be_an(Array)
+    expect(parsed_user["peers"].count).to eq(1)
+    expect(parsed_user["peers"].first["email"]).to eq(user.peers.first.email)
+    expect(parsed_user["peers"].first["first_name"]).to eq(user.peers.first.first_name)
+    expect(parsed_user["peers"].first["last_name"]).to eq(user.peers.first.last_name)
+    expect(parsed_user["peers"].first["image_url"]).to eq(user.peers.first.image_url)
+    expect(parsed_user["peers"].first["linkedin_url"]).to eq(user.peers.first.linkedin_url)
   end
 end
