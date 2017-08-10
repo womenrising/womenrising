e90469e2ec71e652963dffe699354a071644b0ed
Rebecca <rfjackson11@gmail.com>
2017-08-10 14:10:17 -0600

    not sure how this got renamed, but okay

--- spec/features/request_a_mentor_spec.rb
+++ spec/features/request_a_mentor_spec.rb
@@ -10,7 +10,7 @@ feature 'User can request a mentor' do
 
     click_on("Request a Mentor")
     expect(page).to have_content "What would you like to talk with your mentor about?"
-    fill_in 'mentorship_question', with: 'This is a question'
+    fill_in 'mentorship[question]', with: 'This is a question'
     expect{click_on 'Submit'}.to change{Mentorship.count}.by(1)
     expect(page).to have_content 'Your request for a mentor has been submitted.'
   end
