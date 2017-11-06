1ac3e166dec3fc9d52b865747223fcb5b9a01fcf
Benjamin West <mrgenixus@gmail.com>
2017-11-06 15:59:23 -0700

    fix button, tests

--- spec/features/mentorships_index_spec.rb
+++ spec/features/mentorships_index_spec.rb
@@ -20,17 +20,17 @@ feature "I want to see a list of past mentorships", focus: true do
 
     click_on "Mentorship History"
 
-    within '.row', text: mentorship1.mentor.full_name do
+    within 'tr', text: mentorship1.mentor.full_name do
       expect(page).to have_content mentorship1.mentor.full_name
       expect(page).to have_content user.full_name
-      expect(page).to have_content I18n.l(mentorship1.created_at, format: :default)
+      expect(page).to have_content I18n.l(mentorship1.created_at.to_date, format: :default)
       expect(page).to have_content mentorship1.question
     end
 
-    within '.row', text: mentorship2.mentee.full_name do
+    within 'tr', text: mentorship2.mentee.full_name do
       expect(page).to have_content mentorship2.mentee.full_name
       expect(page).to have_content user.full_name
-      expect(page).to have_content I18n.l(mentorship2.created_at, format: :default)
+      expect(page).to have_content I18n.l(mentorship2.created_at.to_date, format: :default)
       expect(page).to have_content mentorship2.question
     end
   end
