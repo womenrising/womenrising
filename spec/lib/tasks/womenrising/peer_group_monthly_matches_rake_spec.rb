58a39f75946c38f51d9946a99a676c000df2c42d
Tim Kleier <tim.kleier@elic.org>
2018-01-10 20:10:44 +0000

    Update wording on peer group signup reminder email, fix specs

--- spec/lib/tasks/womenrising/peer_group_monthly_matches_rake_spec.rb
+++ spec/lib/tasks/womenrising/peer_group_monthly_matches_rake_spec.rb
@@ -1,21 +1,29 @@
 require "rails_helper"
 
-describe "womenrising:peer_group_signup_reminder" do
+describe "womenrising:peer_group_monthly_match" do
   include_context "rake"
 
-  let!(:users) { create_list :user, 10, is_participating_this_month: false }
+  let(:boulder) { create :location }
+  let!(:users) { create_list :user, 10, :groupable, location: boulder }
+  let!(:mentor) { create :mentor, mentor_limit: 2, mentor_times: 0, is_participating_this_month: true }
 
-  it 'runs three days before the end of the month' do
-    travel_to(DateTime.new(2018,1,28)) do
+  it 'runs on the first day of the month' do
+    travel_to(DateTime.new(2017,8,1)) do
+      expect(User).to receive(:match_peers_and_update_users).and_call_original
+
       subject.invoke
       expect(ActionMailer::Base.deliveries.map(&:to).flatten.count).to eq(10)
+      expect(mentor.reload.mentor_times).to eq(2)
     end
   end
 
   it 'does not run on other days of the month' do
-    travel_to(DateTime.new(20018,1,26)) do
+    travel_to(DateTime.new(20017,1,11)) do
+      expect(User).to_not receive(:match_peers_and_update_users)
+
       subject.invoke
       expect(ActionMailer::Base.deliveries.map(&:to).flatten.count).to eq(0)
+      expect(mentor.reload.mentor_times).to eq(0)
     end
   end
 end
---
+++ spec/lib/tasks/womenrising/peer_group_signup_reminder_rake_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "womenrising:peer_group_signup_reminder" do
+  include_context "rake"
+
+  let!(:users) { create_list :user, 10, is_participating_this_month: false }
+
+  it 'runs three days before the end of the month' do
+    travel_to(DateTime.new(2018,1,28)) do
+      subject.invoke
+      expect(ActionMailer::Base.deliveries.map(&:to).flatten.count).to eq(10)
+    end
+  end
+
+  it 'does not run on other days of the month' do
+    travel_to(DateTime.new(20018,1,26)) do
+      subject.invoke
+      expect(ActionMailer::Base.deliveries.map(&:to).flatten.count).to eq(0)
+    end
+  end
+end
